2a9e6cff65a122b12be91988406256799869b860
trixt63 <xuantung.trinh.63@gmail.com>
2024-09-27 16:59:49 +0700

    edit: remove unused library & use extend

--- src/parse_file.py
+++ src/parse_file.py
@@ -1,16 +1,17 @@
 import fitz
-from PyPDF2 import PdfReader
 import logging
 import json
 
 TITLE_AND_TREE_CONTENTS = 25
+
+NEW_PARAGRAPH_THRESHOLD = 28
 
 SECTION_FONTSIZE = 20
 SUBSECTION_FONTSIZE = 18
 SUBSUBSECTION_FONTSIZE = 16
 
 
-def get_paragraphs():
+def get_paragraphs(pdf_path: str):
     pdf_path = '../data/s3-userguide.pdf'
     pdf = fitz.open(pdf_path)
 
@@ -23,7 +24,7 @@ def get_paragraphs():
         page_paragraphs = []
         _is_new_paragraph = True
 
-        previous_y = None  # To track the Y-coordinate of the last line
+        previous_y = None
         for block in blocks:
             if "lines" in block:
                 for line in block["lines"]:
@@ -31,17 +32,18 @@ def get_paragraphs():
                         font_size = span["size"]
                         text = span["text"].strip()
                         page_number = _page_nth - TITLE_AND_TREE_CONTENTS + 1
+
                         # Page header/footer
                         if font_size == 8:
                             continue
 
-                        line_counter += 1  # Increment for each new line in the page
-                        current_y = line["bbox"][1]  # Top Y-coordinate of the line
+                        line_counter += 1
+                        current_y = line["bbox"][1]
                         # Section
                         if font_size == SECTION_FONTSIZE:
                             sections.append({
                                 "page_number": page_number,
-                                "line_number": line_counter,  # Track exact line number
+                                "line_number": line_counter,
                                 "title": text,
                                 "subsections": []
                             })
@@ -51,7 +53,7 @@ def get_paragraphs():
                         elif font_size == SUBSECTION_FONTSIZE:
                             sections[-1]["subsections"].append({
                                 "page_number": page_number,
-                                "line_number": line_counter,  # Track exact line number
+                                "line_number": line_counter,
                                 "title": text,
                                 "subsubsections": []
                             })
@@ -59,17 +61,16 @@ def get_paragraphs():
 
                         # Sub-subsection
                         elif font_size == SUBSUBSECTION_FONTSIZE:
-                            # subsubsection_title = subsection_title + " > " + text
                             sections[-1]["subsections"][-1]["subsubsections"].append({
                                 "page_number": page_number,
-                                "line_number": line_counter,  # Track exact line number
+                                "line_number": line_counter,
                                 "title": text,
                             })
                             _is_new_paragraph = True
 
                         # paragraph
                         else:
-                            if previous_y is not None and abs(current_y - previous_y) > 28.0:  # Example threshold
+                            if previous_y is not None and abs(current_y - previous_y) > NEW_PARAGRAPH_THRESHOLD:
                                 _is_new_paragraph = True
                             if not _is_new_paragraph:
                                 page_paragraphs[-1]["text"] += text
@@ -93,7 +94,7 @@ def get_paragraphs():
 
                         previous_y = current_y
 
-        paragraphs.append(page_paragraphs)
+        paragraphs.extend(page_paragraphs)
 
     pdf.close()
     logging.info(f"number of sections: {len(sections)}")
@@ -101,6 +102,7 @@ def get_paragraphs():
 
 
 if __name__ == '__main__':
-    paragraphs = get_paragraphs()
+    pdf_path = '../data/s3-userguide.pdf'
+    paragraphs = get_paragraphs(pdf_path)
     with open('../data/paragraphs.json', 'w') as f:
         json.dump(paragraphs, f, indent=2)
